76e79bda8cdbee74db9f6bd81fffd2eee6ef75eb
Andreea B <burada.andreea@outlook.com>
2023-02-03 15:38:04 +0200

    Added exam 2023 B rest var-3

---
+++ exam_prep/express/2023_subjects/[B]feb-03_var-3/app.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize('sqlite:memory')
+
+class Device extends Sequelize.Model { };
+
+Device.init({
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: Sequelize.STRING,
+    },
+    price: {
+        type: Sequelize.FLOAT,
+    }
+}, {
+    sequelize,
+    modelName: 'devices',
+    timestamps: false
+});
+
+const app = express();
+app.use(express.json());
+
+app.get('/create', async (req, res) => {
+    await sequelize.sync({force: true});
+    for(let i = 0; i < 10; i++) {
+        await Device.create({name: `Device-${i}`, price: `${Math.random() * 100 + i + 10}`});
+    }
+    res.status(201).json({message: 'devices created'});
+})
+
+app.get('/device', async (req, res) => {
+    const devices = await Device.findAll();
+    res.status(200).send(devices);
+})
+
+app.post('/device', async (req, res) => {
+  // POST /device returns status code 400 and response {message: 'bad request'} if body
+  // is null (0.5 pts);
+  let body = req.body;
+  if (Object.keys(body).length === 0) {
+    return res.status(400).json({ message: "bad request" });
+  }
+
+  // POST /device returns status code 400 and response {message: 'bad request'} if price < 0 (0.5 pts);
+  let price = body.price;
+  if (price < 0) {
+    return res.status(400).json({ message: "bad request" });
+  }
+
+  // POST /device returns status code 400 and response {message: 'bad request'} if
+  // name contains less than 4 characters (0.5 pts);
+  let name = body.name;
+  if (name.length < 5) {
+    return res.status(400).json({ message: "bad request" });
+  }
+
+  // POST /device returns status code 201 and response {message: "device created"} if body
+  // is valid (0.5 pts);
+  let new_device = new Device(body);
+  await new_device.save();
+  return res.status(201).json({ message: "device created" });
+})
+
+app.delete('/device/:id', async (req, res) => {
+  // DELETE /device/:id returns status code 202 and response {message: "device deleted"} if 
+  // id for a device is present in the database (0.5 pts);
+  let device = await Device.findOne({
+    where: {
+      id: req.params.id,
+    },
+  });
+  device.destroy();
+  return res.status(202).json({ message: "device deleted" });
+})
+
+module.exports = app;
