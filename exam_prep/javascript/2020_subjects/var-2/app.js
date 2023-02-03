e4d8396b5567950bc91c57baa60cf3c7841cde84
Andreea B <burada.andreea@outlook.com>
2023-02-03 18:53:53 +0200

    Added 2020 js var-2

---
+++ exam_prep/javascript/2020_subjects/var-2/app.js
@@ -0,0 +1,36 @@
+class Duck{
+    constructor(name){
+        if(typeof name ==='string' || name instanceof String)
+        this.name = name
+        else{
+            throw new Error ('name must be string or String')
+        }
+    }
+    move(){
+        return `${this.name} is moving`
+    }
+    swim(){
+        return `${this.name} is swimming`
+    }
+}
+
+class RubberDuck extends Duck{
+    constructor(name){
+       super(name)
+    }
+    float(){
+        return `${this.name} floats`
+    }
+    swim(){
+        return `${this.name} can't swim, only float`
+    }
+}
+
+
+
+const app = {
+    Duck,
+    RubberDuck
+}
+
+module.exports = app
